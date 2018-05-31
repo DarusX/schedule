93cff0ff2f2e913c9c0aeaef781977e5fe30ed80
Fabián Montero <mntr_rdrgz@hotmail.com>
2018-05-31 16:08:34 -0500

    migraciones

---
+++ database/migrations/2018_05_31_183913_create_classes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateClassesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('classes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('language_id')->unsigned();
+            $table->integer('level');
+            $table->integer('hour_id')->unsigned();
+            $table->integer('classroom_id')->unsigned();
+            $table->integer('professor_id')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('classes');
+    }
+}
